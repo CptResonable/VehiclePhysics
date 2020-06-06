dbb855b622f4c30e27779c1597f78649c8fe5729
CptResonable <50586798+CptResonable@users.noreply.github.com>
2020-06-06 11:48:14 +0200

    Can now detect multiple colliderss

--- Vehicles/Assets/Scripts/CollisionDetector.cs
+++ Vehicles/Assets/Scripts/CollisionDetector.cs
@@ -4,29 +4,40 @@ using UnityEngine;
 
 public class CollisionDetector : MonoBehaviour {
     [SerializeField] GameObject contactMarker;
-    public ContactPoint[] contacts = new ContactPoint[0]{};
+    List<Collision> collisions = new List<Collision>();
+    public List<ContactPoint> contacts = new List<ContactPoint>();
     List<GameObject> markers = new List<GameObject>();
 
-    private void OnCollisionEnter(Collision collision) {
-    }
+    bool firstCollider = true;
+
     private void OnCollisionStay(Collision collision) {
-        Debug.Log(collision.contactCount);
-        contacts = collision.contacts;
+        //Debug.Log(collision.contactCount);
+        if (firstCollider) {
+            contacts = new List<ContactPoint>();
+            firstCollider = false;
+        }
+        foreach (ContactPoint contact in collision.contacts) {
+            contacts.Add(contact);
+        }
         DebugMarkers();
     }
 
+    private void LateUpdate() {
+        firstCollider = true;
+    }
+
     void DebugMarkers() {
-        while (markers.Count != contacts.Length) {
-            if(markers.Count > contacts.Length) {
-                Destroy(markers[markers.Count - 1]);
-                markers.Remove(markers[markers.Count - 1]);
-            }
-            else {
-                markers.Add(Instantiate(contactMarker));
-            }
-        }
-        for (int i = 0; i < markers.Count; i++) {
-            markers[i].transform.position = contacts[i].point;
-        }
+        //while (markers.Count != contacts.Length) {
+        //    if(markers.Count > contacts.Length) {
+        //        Destroy(markers[markers.Count - 1]);
+        //        markers.Remove(markers[markers.Count - 1]);
+        //    }
+        //    else {
+        //        markers.Add(Instantiate(contactMarker));
+        //    }
+        //}
+        //for (int i = 0; i < markers.Count; i++) {
+        //    markers[i].transform.position = contacts[i].point;
+        //}
     }
 }
